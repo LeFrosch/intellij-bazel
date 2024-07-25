38abd9500cd9d14d068b298edb5b432ae93c03c8
Daniel Brauner <daniel.brauner@jetbrains.com>
2024-07-25 08:52:50 +0200

    updated sync output

--- clwb/tests/integrationtests/com/google/idea/blaze/clwb/base/SyncOutput.java
+++ clwb/tests/integrationtests/com/google/idea/blaze/clwb/base/SyncOutput.java
@@ -5,44 +5,59 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 import com.google.idea.blaze.base.scope.BlazeContext;
 import com.google.idea.blaze.base.scope.OutputSink;
+import com.google.idea.blaze.base.scope.OutputSink.Propagation;
 import com.google.idea.blaze.base.scope.output.IssueOutput;
+import com.google.idea.blaze.base.scope.output.StatusOutput;
+import com.google.idea.blaze.base.scope.output.SummaryOutput;
 import com.google.idea.blaze.common.Output;
 import com.google.idea.blaze.common.PrintOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
 
 public class SyncOutput {
-  private static class BufferSink<T extends Output> implements OutputSink<T> {
-    private final List<T> buffer = new ArrayList<>();
-
-    @Override
-    public Propagation onOutput(@NotNull T output) {
-      buffer.add(output);
-      return Propagation.Continue;
-    }
-  }
-
-  private final BufferSink<IssueOutput> issues = new BufferSink<>();
-  private final BufferSink<PrintOutput> prints = new BufferSink<>();
+  private final List<IssueOutput> issues = new ArrayList<>();
+  private final List<String> messages = new ArrayList<>();
 
   void install(BlazeContext context) {
-    context.addOutputSink(IssueOutput.class, issues);
-    context.addOutputSink(PrintOutput.class, prints);
+    addOutputSink(context, IssueOutput.class, issues::add);
+    addOutputSink(context, PrintOutput.class, (it) -> messages.add(it.getText()));
+    addOutputSink(context, StatusOutput.class, (it) -> messages.add(it.getStatus()));
+    addOutputSink(context, SummaryOutput.class, (it) -> messages.add(it.getText()));
+  }
+
+  private <T extends Output> void addOutputSink(BlazeContext context, Class<T> clazz, Consumer<T> consumer) {
+    context.addOutputSink(clazz, (it) -> {
+      consumer.accept(it);
+      return Propagation.Continue;
+    });
   }
 
   public String collectLog() {
-    return prints.buffer.stream()
-        .map(PrintOutput::getText)
-        .collect(Collectors.joining("\n"));
+    final var builder = new StringBuilder();
+    final var separator = String.format("%n%s%n", "=".repeat(100));
+
+    builder.append(separator);
+    for (final var element : System.getenv().entrySet()) {
+      builder.append(String.format("%s: %s%n", element.getKey(), element.getValue()));
+    }
+
+    builder.append(separator);
+    for (int i = 0; i < messages.size(); i++) {
+      builder.append(String.format("%03d: %s%n", i, messages.get(i)));
+    }
+
+    return builder.toString();
   }
 
   public void assertNoErrors() {
-    final var log = collectLog();
+    final var message = String.format(
+        "there where errors during the sync, check this log:%n%s",
+        collectLog()
+    );
 
-    assertWithMessage("There where errors during the sync, check this log:\n" + log)
-        .that(issues.buffer)
-        .isEmpty();
+    assertWithMessage(message).that(issues).isEmpty();
   }
 }
