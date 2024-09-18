6083b6d88392bf88663ba54cfdd679783aa7d104
Daniel Brauner <daniel.brauner@jetbrains.com>
2024-09-18 14:51:01 +0200

    added symbols to builtins

--- base/src/resources/pylark/starlark.py
+++ base/src/resources/pylark/starlark.py
@@ -1,1 +1,27 @@
 from .default import *
+
+def rule(
+  implementation,
+  *,
+  test=None,
+  attrs={},
+  outputs=None,
+  executable=unbound,
+  output_to_genfiles=False,
+  fragments=[],
+  host_fragments=[],
+  _skylark_testable=False,
+  toolchains=[],
+  incompatible_use_toolchain_transition=False,
+  doc=None,
+  provides=[],
+  exec_compatible_with=[],
+  analysis_test=False,
+  build_setting=None,
+  cfg=None,
+  exec_groups=None,
+  initializer=None,
+  parent=None,
+  extendable=None,
+  subrules=[],
+): ...
