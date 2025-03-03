d985baae072a7a96cd07c4f869533f591fd47489
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-03-03 17:12:11 +0100

    added xcode support

--- cpp/src/com/google/idea/blaze/cpp/qsync/CcCompilerInfoCollectorImpl.kt
+++ cpp/src/com/google/idea/blaze/cpp/qsync/CcCompilerInfoCollectorImpl.kt
@@ -15,7 +15,11 @@
  */
 package com.google.idea.blaze.cpp.qsync
 
+import com.google.idea.blaze.base.projectview.ProjectViewManager
+import com.google.idea.blaze.base.projectview.section.sections.BazelBinarySection
 import com.google.idea.blaze.base.qsync.CcCompilerInfoCollectorProvider
+import com.google.idea.blaze.base.settings.BlazeUserSettings
+import com.google.idea.blaze.base.sync.aspects.storage.AspectRepositoryProvider
 import com.google.idea.blaze.base.util.pluginProjectScope
 import com.google.idea.blaze.common.Context
 import com.google.idea.blaze.common.PrintOutput
@@ -31,8 +35,16 @@ import com.intellij.platform.eel.executeProcess
 import com.intellij.platform.eel.getOrThrow
 import com.intellij.platform.eel.impl.utils.awaitProcessResult
 import com.intellij.platform.eel.provider.getEelDescriptor
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.InvalidPathException
 import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import java.util.*
 
 private val LOG = logger<CcCompilerInfoCollectorImpl>()
 
@@ -60,8 +72,7 @@ class CcCompilerInfoCollectorImpl private constructor(
 
   @Throws(BuildException::class)
   override fun getXcodeData(ctx: Context<*>, toolchain: CcToolchain): ProjectProto.XcodeData {
-    // TODO: implement xcode data collection (see com.google.idea.blaze.cpp.XcodeCompilerSettingsProviderImpl)
-    return ProjectProto.XcodeData.newBuilder().build()
+    return runJob(ctx, toolchain, ExecutionContext::getXcodeData)
   }
 
   @Throws(BuildException::class)
@@ -101,6 +112,27 @@ private class ExecutionContext(
     LOG.info(msg)
     ctx.output(PrintOutput.log(msg))
   }
+
+  val bazelBinary: Path by lazy {
+    val projectSpecificBinary = getProjectSpecificBazelBinary()
+    if (projectSpecificBinary != null) {
+      return@lazy projectSpecificBinary
+    }
+
+    try {
+      Path.of(BlazeUserSettings.getInstance().bazelBinaryPath)
+    } catch (e: InvalidPathException) {
+      throw BuildException("invalid bazel path", e)
+    }
+  }
+
+  private fun getProjectSpecificBazelBinary(): Path? {
+    val projectView = ProjectViewManager.getInstance(project).projectViewSet ?: return null
+
+    return projectView.getScalarValue(BazelBinarySection.KEY)
+      .map(File::toPath)
+      .orElse(null)
+  }
 }
 
 @Suppress("UnstableApiUsage") // EEL API is still unstable
@@ -133,5 +165,100 @@ private fun guessCompilerKind(version: String): ProjectProto.CcCompilerKind {
     version.contains("gcc") -> ProjectProto.CcCompilerKind.GCC
     version.contains("Microsoft") -> ProjectProto.CcCompilerKind.MSVC
     else -> ProjectProto.CcCompilerKind.CC_COMPILER_KIND_UNKNOWN
+  }
+}
+
+private val XCODE_QUERY_TEMPLATE =
+  "cquery 'deps(@bazel_tools//tools/osx:current_xcode_config)' --output=starlark --starlark:file='%s'"
+
+@Suppress("UnstableApiUsage") // EEL API is still unstable
+private suspend fun ExecutionContext.getXcodeData(): ProjectProto.XcodeData {
+  val queryFileSource = AspectRepositoryProvider.aspectQSyncFile("xcode_query.bzl")
+
+  val queryFile = try {
+    withContext(Dispatchers.IO) {
+      val tmpFile = Files.createTempFile("xcode_query", ".bzl")
+      Files.copy(queryFileSource.openStream(), tmpFile, StandardCopyOption.REPLACE_EXISTING)
+
+      tmpFile
+    }
+  } catch (e: IOException) {
+    throw BuildException("could not copy xcode query file", e)
+  }
+
+  val result = project.getEelDescriptor().upgrade().exec
+    .executeProcess(bazelBinary.toString(), String.format(XCODE_QUERY_TEMPLATE, queryFile.toString()))
+    .getOrThrow { BuildException("could not execute xcode query: ${it.message}") }
+    .awaitProcessResult()
+
+  if (result.exitCode == 37) {
+    // if the target `@bazel_tools//tools/osx:xcode-locator` doesn't exist, bazel will crash with error code 37
+    return ProjectProto.XcodeData.getDefaultInstance()
+  }
+
+  if (result.exitCode != 0) {
+    throw BuildException("xcode query failed: ${result.stderr}")
+  }
+
+  val config = parseXcodeConfig(result.stdout)
+
+  val developerDir = if (config.commandLineToolsOnly) {
+    Path.of("/Library/Developer/CommandLineTools")
+  } else {
+    getDeveloperDir(config.version!!)
+  }
+
+  val sdkRoot = Path.of(developerDir.toString(), "SDKs", "MacOSX.sdk")
+
+  return ProjectProto.XcodeData.newBuilder()
+    .setDeveloperDir(developerDir.toString())
+    .setSdkRoot(sdkRoot.toString())
+    .build()
+}
+
+private data class XcodeConfig(val version: String?, val sdkVersion: String, val commandLineToolsOnly: Boolean)
+
+private fun parseXcodeConfig(stdout: String): XcodeConfig {
+  for (line in stdout.lines()) {
+    if (line.isBlank()) continue
+
+    val versions = line.split(' ')
+    if (versions.size != 2) {
+      throw BuildException("could not parse xcode versions: $line")
+    }
+
+    // if you only have CommandLineTools installed the query will fail to fetch an xcode version,
+    // but it will still return an SDK version
+    if (versions[0] == "None") {
+      return XcodeConfig(null, versions[1], true)
+    }
+
+    // Using the first occurrence is fine because the target we query is an alias for the current config anyway
+    return XcodeConfig(versions[0], versions[1], false)
+  }
+
+  throw BuildException("no usable scode version returned by query: $stdout")
+}
+
+/**
+ * Pass the version to the xcode locator, so that it returns the developer dir. This is a mirror
+ * of Bazel's own behavior: Ref:
+ * https://github.com/bazelbuild/bazel/blob/1811e82ca4e68c2dd52eed7907c3d1926237e18a/src/main/java/com/google/devtools/build/lib/exec/local/XcodeLocalEnvProvider.java#L241
+ */
+private suspend fun ExecutionContext.getDeveloperDir(version: String): Path {
+  // TODO: add flags from project view to avoid invalidation
+  val result = project.getEelDescriptor().upgrade().exec
+    .executeProcess(bazelBinary.toString(), "run @bazel_tools//tools/osx:xcode-locator -- $version")
+    .getOrThrow { BuildException("could not execute xcode query: ${it.message}") }
+    .awaitProcessResult()
+
+  if (result.exitCode != 0) {
+    throw BuildException("could not locate xcode: ${result.stderr}")
+  }
+
+  return try {
+    Path.of(result.stdout.trim())
+  } catch (e: InvalidPathException) {
+    throw BuildException("invalid xcode location: ${result.stdout}")
   }
 }
