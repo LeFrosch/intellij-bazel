a1b9d3ff37bd52497a6c43a4347aeaf1356c9095
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-07-18 19:13:09 +0200

    wip tests

--- clwb/tests/headlesstests/com/google/idea/blaze/clwb/VirtualIncludesCacheTest.java
+++ clwb/tests/headlesstests/com/google/idea/blaze/clwb/VirtualIncludesCacheTest.java
@@ -2,23 +2,18 @@ package com.google.idea.blaze.clwb;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.idea.blaze.clwb.base.Assertions.assertContainsHeader;
+import static com.google.idea.blaze.clwb.base.Assertions.assertCachedHeader;
 
-import com.google.idea.blaze.clwb.base.AllowedVfsRoot;
 import com.google.idea.blaze.clwb.base.ClwbHeadlessTestCase;
-import com.google.idea.blaze.clwb.base.TestUtils;
 import com.google.idea.testing.headless.BazelVersionRule;
 import com.intellij.openapi.util.registry.Registry;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.jetbrains.cidr.lang.workspace.OCCompilerSettings;
-import java.util.ArrayList;
-import org.jetbrains.annotations.Nullable;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class VirtualIncludesTest extends ClwbHeadlessTestCase {
+public class VirtualIncludesCacheTest extends ClwbHeadlessTestCase {
 
   // protobuf requires bazel 7+
   @Rule
@@ -26,8 +21,8 @@ public class VirtualIncludesTest extends ClwbHeadlessTestCase {
 
   @Test
   public void testClwb() {
-    Registry.get("bazel.cpp.sync.allow.bazel.bin.header.search.path").setValue(true);
-    Registry.get("bazel.cc.virtual.includes.cache.enabled").setValue(false);
+    Registry.get("bazel.cpp.sync.allow.bazel.bin.header.search.path").setValue(false);
+    Registry.get("bazel.cc.virtual.includes.cache.enabled").setValue(true);
 
     final var errors = runSync(defaultSyncParams().build());
     errors.assertNoErrors();
@@ -35,14 +30,6 @@ public class VirtualIncludesTest extends ClwbHeadlessTestCase {
     checkIncludes();
     checkImplDeps();
     checkProto();
-  }
-
-  @Override
-  protected void addAllowedVfsRoots(ArrayList<AllowedVfsRoot> roots) {
-    super.addAllowedVfsRoots(roots);
-    roots.add(AllowedVfsRoot.bazelBinRecursive(myBazelInfo, "lib/strip_absolut/_virtual_includes"));
-    roots.add(AllowedVfsRoot.bazelBinRecursive(myBazelInfo, "lib/transitive"));
-    roots.add(AllowedVfsRoot.bazelBinRecursive(myBazelInfo, "proto"));
   }
 
   private void checkIncludes() {
@@ -56,23 +43,13 @@ public class VirtualIncludesTest extends ClwbHeadlessTestCase {
     assertContainsHeader("raw_quote.h", compilerSettings);
     assertContainsHeader("lib/transitive/generated.h", compilerSettings);
 
-    assertThat(findProjectFile("lib/strip_absolut/strip_absolut.h"))
-        .isEqualTo(TestUtils.resolveHeader("strip_absolut/strip_absolut.h", compilerSettings));
-
-    assertThat(findProjectFile("lib/strip_relative/include/strip_relative.h"))
-        .isEqualTo(TestUtils.resolveHeader("strip_relative.h", compilerSettings));
-
-    assertThat(findProjectFile("lib/impl_deps/impl.h"))
-        .isEqualTo(TestUtils.resolveHeader("lib/impl_deps/impl.h", compilerSettings));
-
-    assertThat(findProjectFile("lib/raw_files/default/raw_default.h"))
-        .isEqualTo(TestUtils.resolveHeader("raw_default.h", compilerSettings));
-
-    assertThat(findProjectFile("lib/raw_files/system/raw_system.h"))
-        .isEqualTo(TestUtils.resolveHeader("raw_system.h", compilerSettings));
-
-    assertThat(findProjectFile("lib/raw_files/quote/raw_quote.h"))
-        .isEqualTo(TestUtils.resolveHeader("raw_quote.h", compilerSettings));
+    assertCachedHeader("strip_absolut/strip_absolut.h", compilerSettings, myProject);
+    assertCachedHeader("strip_relative.h", compilerSettings, myProject);
+    // assertCachedHeader("lib/impl_deps/impl.h", compilerSettings, myProject);
+    // assertCachedHeader("raw_default.h", compilerSettings, myProject);
+    // assertCachedHeader("raw_system.h", compilerSettings, myProject);
+    // assertCachedHeader("raw_quote.h", compilerSettings, myProject);
+    assertCachedHeader("lib/transitive/generated.h", compilerSettings, myProject);
   }
 
   private void checkImplDeps() {
@@ -91,5 +68,6 @@ public class VirtualIncludesTest extends ClwbHeadlessTestCase {
     assertThat(headersSearchRoots).isNotEmpty();
 
     assertContainsHeader("proto/addressbook.pb.h", compilerSettings);
+    assertCachedHeader("proto/addressbook.pb.h", compilerSettings, myProject);
   }
 }
