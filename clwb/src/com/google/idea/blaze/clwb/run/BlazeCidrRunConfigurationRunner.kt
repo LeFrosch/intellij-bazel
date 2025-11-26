3e52afcd74c9458ba67292d36918685e440b9280
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-11-26 16:43:13 +0100

    converted BlazeCidrRunConfigurationRunner to kotlin

--- clwb/src/com/google/idea/blaze/clwb/run/BlazeCidrRunConfigurationRunner.kt
+++ clwb/src/com/google/idea/blaze/clwb/run/BlazeCidrRunConfigurationRunner.kt
@@ -13,140 +13,111 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.idea.blaze.clwb.run;
+package com.google.idea.blaze.clwb.run
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.idea.blaze.base.command.BlazeCommandName;
-import com.google.idea.blaze.base.command.BlazeInvocationContext;
-import com.google.idea.blaze.base.command.buildresult.GetArtifactsException;
-import com.google.idea.blaze.base.command.buildresult.BuildResultParser;
-import com.google.idea.blaze.base.command.buildresult.bepparser.BuildEventStreamProvider;
-import com.google.idea.blaze.base.model.primitives.Label;
-import com.google.idea.blaze.base.model.primitives.TargetExpression;
-import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
-import com.google.idea.blaze.base.run.BlazeBeforeRunCommandHelper;
-import com.google.idea.blaze.base.run.BlazeCommandRunConfiguration;
-import com.google.idea.blaze.base.run.ExecutorType;
-import com.google.idea.blaze.base.run.confighandler.BlazeCommandRunConfigurationRunner;
-import com.google.idea.blaze.base.util.SaveUtil;
-import com.google.idea.blaze.common.Interners;
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.Executor;
-import com.intellij.execution.RunCanceledByUserException;
-import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.executors.DefaultDebugExecutor;
-import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.runners.ExecutionUtil;
-import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.registry.Registry;
-import com.intellij.util.PathUtil;
-import com.jetbrains.cidr.execution.CidrCommandLineState;
+import com.google.common.collect.ImmutableList
+import com.google.idea.blaze.base.command.BlazeCommandName
+import com.google.idea.blaze.base.command.BlazeInvocationContext
+import com.google.idea.blaze.base.command.buildresult.BuildResultParser
+import com.google.idea.blaze.base.command.buildresult.GetArtifactsException
+import com.google.idea.blaze.base.model.primitives.Label
+import com.google.idea.blaze.base.model.primitives.WorkspaceRoot
+import com.google.idea.blaze.base.run.BlazeBeforeRunCommandHelper
+import com.google.idea.blaze.base.run.BlazeCommandRunConfiguration
+import com.google.idea.blaze.base.run.ExecutorType
+import com.google.idea.blaze.base.run.confighandler.BlazeCommandRunConfigurationRunner
+import com.google.idea.blaze.base.util.SaveUtil
+import com.google.idea.blaze.clwb.run.BlazeGDBServerProvider.getFlagsForDebugging
+import com.google.idea.blaze.clwb.run.BlazeGDBServerProvider.getOptionalFissionArguments
+import com.google.idea.blaze.common.Interners
+import com.intellij.execution.ExecutionException
+import com.intellij.execution.Executor
+import com.intellij.execution.RunCanceledByUserException
+import com.intellij.execution.configurations.RunProfileState
+import com.intellij.execution.executors.DefaultDebugExecutor
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.runners.ExecutionUtil
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.util.PathUtil
+import com.intellij.util.asSafely
+import com.jetbrains.cidr.execution.CidrCommandLineState
+import com.jetbrains.cidr.lang.toolchains.CidrSwitchBuilder
+import com.jetbrains.cidr.lang.workspace.compiler.ClangClCompilerKind
+import com.jetbrains.cidr.lang.workspace.compiler.MSVCCompilerKind
+import java.io.File
+import java.util.concurrent.CancellationException
+import java.util.function.Consumer
 
-import com.jetbrains.cidr.lang.toolchains.CidrSwitchBuilder;
-import com.jetbrains.cidr.lang.workspace.compiler.ClangClCompilerKind;
-import com.jetbrains.cidr.lang.workspace.compiler.ClangCompilerKind;
-import com.jetbrains.cidr.lang.workspace.compiler.MSVCCompilerKind;
-import javax.annotation.Nullable;
-import java.io.File;
-import java.util.List;
-import java.util.concurrent.CancellationException;
+/** CLion-specific handler for [BlazeCommandRunConfiguration]s.  */
+class BlazeCidrRunConfigurationRunner(private val configuration: BlazeCommandRunConfiguration) :
+  BlazeCommandRunConfigurationRunner {
 
-/** CLion-specific handler for {@link BlazeCommandRunConfiguration}s. */
-public class BlazeCidrRunConfigurationRunner implements BlazeCommandRunConfigurationRunner {
+  /** Calculated during the before-run task, and made available to the debugger.  */
+  private var executableToDebug: File? = null
 
-  private final BlazeCommandRunConfiguration configuration;
-
-  /** Calculated during the before-run task, and made available to the debugger. */
-  File executableToDebug = null;
-
-  BlazeCidrRunConfigurationRunner(BlazeCommandRunConfiguration configuration) {
-    this.configuration = configuration;
+  override fun getRunProfileState(executor: Executor, env: ExecutionEnvironment): RunProfileState {
+    return CidrCommandLineState(env, BlazeCidrLauncher(configuration, this, env))
   }
 
-  @Override
-  public RunProfileState getRunProfileState(Executor executor, ExecutionEnvironment env) {
-    return new CidrCommandLineState(env, new BlazeCidrLauncher(configuration, this, env));
-  }
+  override fun executeBeforeRunTask(env: ExecutionEnvironment): Boolean {
+    executableToDebug = null
+    if (!isDebugging(env)) return true
 
-  @Override
-  public boolean executeBeforeRunTask(ExecutionEnvironment env) {
-    executableToDebug = null;
-    if (!isDebugging(env)) {
-      return true;
-    }
     try {
-      File executable = getExecutableToDebug(env);
-      if (executable != null) {
-        executableToDebug = executable;
-        return true;
-      }
-    } catch (ExecutionException e) {
-      ExecutionUtil.handleExecutionError(
-          env.getProject(), env.getExecutor().getToolWindowId(), env.getRunProfile(), e);
+      val executable = getExecutableToDebug(env)
+      executableToDebug = executable
+      return true
+    } catch (e: ExecutionException) {
+      ExecutionUtil.handleExecutionError(env.project, env.executor.toolWindowId, env.runProfile, e)
+      return false
     }
-    return false;
   }
 
-  private static boolean isDebugging(ExecutionEnvironment environment) {
-    Executor executor = environment.getExecutor();
-    return executor instanceof DefaultDebugExecutor;
-  }
-
-  private static Label getSingleTarget(BlazeCommandRunConfiguration config)
-      throws ExecutionException {
-    ImmutableList<? extends TargetExpression> targets = config.getTargets();
-    if (targets.size() != 1 || !(targets.get(0) instanceof Label)) {
-      throw new ExecutionException("Invalid configuration: doesn't have a single target label");
-    }
-    return (Label) targets.get(0);
-  }
-
-  private ImmutableList<String> getExtraDebugFlags(ExecutionEnvironment env) {
-    if (Registry.is("bazel.clwb.debug.extraflags.disabled")) {
-      return ImmutableList.of();
+  private fun getExtraDebugFlags(env: ExecutionEnvironment): ImmutableList<String> {
+    if (Registry.`is`("bazel.clwb.debug.extraflags.disabled")) {
+      return ImmutableList.of()
     }
 
-    final var debuggerKind = RunConfigurationUtils.getDebuggerKind(configuration);
+    val debuggerKind = RunConfigurationUtils.getDebuggerKind(configuration)
     if (debuggerKind == BlazeDebuggerKind.GDB_SERVER) {
-      return BlazeGDBServerProvider.getFlagsForDebugging(configuration.getHandler().getState());
+      return getFlagsForDebugging(configuration.handler.getState())
     }
 
-    final var flagsBuilder = ImmutableList.<String>builder();
-    if (Registry.is("bazel.clwb.debug.enforce.dbg.compilation.mode")) {
-      flagsBuilder.add("--compilation_mode=dbg");
-      flagsBuilder.add("--strip=never");
-      flagsBuilder.add("--dynamic_mode=off");
+    val flagsBuilder = ImmutableList.builder<String>()
+    if (Registry.`is`("bazel.clwb.debug.enforce.dbg.compilation.mode")) {
+      flagsBuilder.add("--compilation_mode=dbg")
+      flagsBuilder.add("--strip=never")
+      flagsBuilder.add("--dynamic_mode=off")
     }
 
-    var compilerKind = RunConfigurationUtils.getCompilerKind(configuration);
-    if (compilerKind == ClangClCompilerKind.INSTANCE || compilerKind == MSVCCompilerKind.INSTANCE) {
-      var sb = new CidrSwitchBuilder();
-      var sb2 = compilerKind.getSwitchBuilder(sb);
+    val compilerKind = RunConfigurationUtils.getCompilerKind(configuration)
+    if (compilerKind === ClangClCompilerKind || compilerKind === MSVCCompilerKind) {
+      val sb = CidrSwitchBuilder()
+      val sb2 = compilerKind.getSwitchBuilder(sb)
       sb2
-          .withDebugInfo(-1) // ignored for msvc/clangcl
-          .withDisableOptimization();
+        .withDebugInfo(-1) // ignored for msvc/clangcl
+        .withDisableOptimization()
 
-      sb2.buildRaw().forEach((opt) -> {
-        flagsBuilder.add("--copt=" + opt);
-      });
-
+      sb2.buildRaw().forEach(Consumer { opt: String? ->
+        flagsBuilder.add("--copt=$opt")
+      })
     } else {
-      if (debuggerKind == BlazeDebuggerKind.BUNDLED_LLDB && !Registry.is(
-          "bazel.trim.absolute.path.disabled")) {
+      if (debuggerKind == BlazeDebuggerKind.BUNDLED_LLDB && !Registry.`is`("bazel.trim.absolute.path.disabled")) {
         flagsBuilder.add(
-            "--copt=-fdebug-compilation-dir=" + WorkspaceRoot.fromProject(env.getProject()));
+          "--copt=-fdebug-compilation-dir=" + WorkspaceRoot.fromProject(env.project)
+        )
 
         if (SystemInfo.isMac) {
-          flagsBuilder.add("--linkopt=-Wl,-oso_prefix,.");
+          flagsBuilder.add("--linkopt=-Wl,-oso_prefix,.")
         }
       }
 
-      flagsBuilder.add("--copt=-O0");
-      flagsBuilder.add("--copt=-g");
-      flagsBuilder.addAll(BlazeGDBServerProvider.getOptionalFissionArguments());
+      flagsBuilder.add("--copt=-O0")
+      flagsBuilder.add("--copt=-g")
+      flagsBuilder.addAll(getOptionalFissionArguments())
     }
-    return flagsBuilder.build();
+    return flagsBuilder.build()
   }
 
   /**
@@ -154,63 +125,65 @@ public class BlazeCidrRunConfigurationRunner implements BlazeCommandRunConfigura
    *
    * @throws ExecutionException if no unique output artifact was found.
    */
-  private File getExecutableToDebug(ExecutionEnvironment env) throws ExecutionException {
-    SaveUtil.saveAllFiles();
+  @Throws(ExecutionException::class)
+  private fun getExecutableToDebug(env: ExecutionEnvironment): File {
+    SaveUtil.saveAllFiles()
 
-    ListenableFuture<BuildEventStreamProvider> streamProviderFuture =
-        BlazeBeforeRunCommandHelper.runBlazeCommand(
-            BlazeCommandName.BUILD,
-            configuration,
-            ImmutableList.of(),
-            getExtraDebugFlags(env),
-            BlazeInvocationContext.runConfigContext(
-                ExecutorType.fromExecutor(env.getExecutor()), configuration.getType(), true),
-            "Building debug binary");
+    // TODO: migrate this to use the BazelExecService and the new build/sync view
+    val eventStreamFuture = BlazeBeforeRunCommandHelper.runBlazeCommand(
+      BlazeCommandName.BUILD,
+      configuration,
+      ImmutableList.of(),
+      getExtraDebugFlags(env),
+      BlazeInvocationContext.runConfigContext(ExecutorType.fromExecutor(env.executor), configuration.type, true),
+      "Building debug binary"
+    )
 
-    Label target = getSingleTarget(configuration);
-    try (BuildEventStreamProvider streamProvider = streamProviderFuture.get()) {
-      final var candidateFiles = BuildResultParser.getExecutableArtifacts(
-          streamProvider,
+    val target: Label = getSingleTarget(configuration)
+    try {
+      eventStreamFuture.get().use { bepStream ->
+        val candidateFiles = BuildResultParser.getExecutableArtifacts(
+          bepStream,
           Interners.STRING,
           target.toString()
-      );
+        )
 
-      File file = findExecutable(target, candidateFiles);
-      if (file == null) {
-        throw new ExecutionException(
-            String.format(
-                "More than 1 executable was produced when building %s; don't know which to debug",
-                target));
+        return findExecutable(target, candidateFiles)
+          ?: throw ExecutionException("More than 1 executable was produced when building $target; don't know which to debug")
       }
-
-      return file;
-    } catch (InterruptedException | CancellationException e) {
-      streamProviderFuture.cancel(true);
-      throw new RunCanceledByUserException();
-    } catch (java.util.concurrent.ExecutionException e) {
-      throw new ExecutionException(e);
-    } catch (GetArtifactsException e) {
-      throw new ExecutionException(
-          String.format(
-              "Failed to get output artifacts when building %s: %s", target, e.getMessage()));
+    } catch (_: InterruptedException) {
+      eventStreamFuture.cancel(true)
+      throw RunCanceledByUserException()
+    } catch (_: CancellationException) {
+      eventStreamFuture.cancel(true)
+      throw RunCanceledByUserException()
+    } catch (e: java.util.concurrent.ExecutionException) {
+      throw ExecutionException(e)
+    } catch (e: GetArtifactsException) {
+      throw ExecutionException("Failed to get output artifacts when building $target: ${e.message}")
     }
   }
 
-  /**
-   * Basic heuristic for choosing between multiple output files. Currently just looks for a filename
-   * matching the target name.
-   */
-  @Nullable
-  private static File findExecutable(Label target, List<File> outputs) {
-    if (outputs.size() == 1) {
-      return outputs.get(0);
+  companion object {
+    private fun isDebugging(environment: ExecutionEnvironment): Boolean {
+      return environment.executor is DefaultDebugExecutor
     }
-    String name = PathUtil.getFileName(target.targetName().toString());
-    for (File file : outputs) {
-      if (file.getName().equals(name)) {
-        return file;
-      }
+
+    @Throws(ExecutionException::class)
+    private fun getSingleTarget(config: BlazeCommandRunConfiguration): Label {
+      return config.targets.firstOrNull()?.asSafely<Label>()
+        ?: throw ExecutionException("Invalid configuration: doesn't have a single target label")
     }
-    return null;
+
+    /**
+     * Basic heuristic for choosing between multiple output files. Currently just looks for a filename
+     * matching the target name.
+     */
+    private fun findExecutable(target: Label, outputs: List<File>): File? {
+      if (outputs.size == 1) return outputs[0]
+
+      val name = PathUtil.getFileName(target.targetName().toString())
+      return outputs.firstOrNull { file -> file.getName() == name }
+    }
   }
 }
